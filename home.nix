926c8b0710bbeba1ac4115021ada83c275b0c028
Ruin0x11 <ipickering2@gmail.com>
2020-08-23 04:58:05 -0700

    Add more dots

--- home.nix
+++ home.nix
@@ -33,11 +33,44 @@
         owner = "VundleVim";
         repo = "Vundle.vim";
         rev = "b255382";
-	sha256 = "0fkmklcq3fgvd6x6irz9bgyvcdaxafykk3k89gsi9p6b0ikw3rw6";
+        sha256 = "0fkmklcq3fgvd6x6irz9bgyvcdaxafykk3k89gsi9p6b0ikw3rw6";
      };
-
      ".vimrc.bundles".source = ./vim/vimrc.bundles;
 
      ".zshrc".source = ./zsh/zshrc;
+     ".zshrc.functions".source = ./zsh/zshrc.functions;
+     ".aliases".source = ./zsh/aliases;
+     ".zsh/zsh-history-substring-search".source = pkgs.fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-history-substring-search";
+        rev = "0f80b8e";
+        sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
+     };
+     ".zsh/zsh-syntax-highlighting".source = pkgs.fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-syntax-highlighting";
+        rev = "4cf464f";
+        sha256 = "0s197576djz1ysk0s5zlmw4v657v0f598mkmqzk32wgs4yvfqabx";
+     };
+     ".zsh/zsh-autosuggestions".source = pkgs.fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-autosuggestions";
+        rev = "ae315de";
+        sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
+     };
+     ".zsh/git.zsh".source = ./zsh/git.zsh;
+
+     ".tmux.conf".source = ./tmux/tmux.conf;
+
+     ".xinitrc".source = ./x11/xinitrc;
+
+     ".bin".source = ./bin;
+  };
+
+  xdg.configFile = {
+    "i3".source = ./i3;
+    "i3blocks".source = ./i3blocks;
+
+    "mpv".source = ./mpv;
   };
 }
---
+++ mpv/scripts/dood.lua
@@ -0,0 +1,17 @@
+function inscribe_thing()
+    if file_exists(mp.get_property("path", nil)) then
+        thing = mp.get_property("media-title", "dood")
+        length = mp.get_property_number("length", 0)
+        file = io.open(os.getenv("HOME") .. "/Dropbox/EXlog.txt", "a+")
+        io.output(file)
+        io.write(thing .. "\t" .. os.date('%m-%d-%y %H:%M:%S') .. length .. "\n")
+        io.close(file)
+    end
+end
+
+function file_exists(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+end
+
+mp.register_event("start-file", inscribe_thing)
